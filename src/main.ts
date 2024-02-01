eb61e886bea14990683694c039eecc066523a40f
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-02-01 18:31:53 +0530

    Pipe added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
@@ -7,6 +8,8 @@ async function bootstrap() {
     const port = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
     console.log('Nest application created and Database connected');
+    app.useGlobalPipes(new ValidationPipe());
+
     await app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
