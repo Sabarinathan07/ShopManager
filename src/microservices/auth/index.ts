1510010c09befd15a0764a6e0a2f76768f311fe6
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-02-09 19:02:04 +0530

    add index files

---
+++ src/microservices/auth/index.ts
@@ -0,0 +1,6 @@
+export * from './auth.module';
+export * from './controllers';
+export * from './helpers';
+export * from './interceptors';
+export * from './interfaces';
+export * from './services';
---
+++ src/microservices/item/index.ts
@@ -0,0 +1,7 @@
+export * from './item.module';
+export * from './controllers';
+export * from './helpers';
+export * from './interceptors';
+export * from './interfaces';
+export * from './services';
+export * from './entity';
